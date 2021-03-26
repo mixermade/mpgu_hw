0316b4e49b6d1bd566885cc1b2526f97749fcf95
mixermade <79300664+mixermade@users.noreply.github.com>
2021-03-26 13:42:24 +0300

    Task 7 complete
    its working ITS WORKING

--- Hw/src/QuadraticSort.java
+++ Hw/src/QuadraticSort.java
@@ -4,6 +4,14 @@ import java.util.Arrays;
 public class QuadraticSort {
     public void sort(int[] data)
     {
-        Arrays.sort(data); // Replace this
+        for (int i=1; i<data.length;i++){
+            int current = data[i];
+            int j = i-1;
+            while (j>= 0&& current < data[j]){
+                data[j+1]=data[j];
+                j--;
+            }
+            data[j+1]=current;
+        }
     }
 }
