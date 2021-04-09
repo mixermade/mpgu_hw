e2b17c8d8d9e8e235336ffdef3a85f220885d0b0
mixermade <79300664+mixermade@users.noreply.github.com>
2021-04-09 07:04:38 +0300

    Update NLogNSort.java
    create method to divide array

--- Hw/src/NLogNSort.java
+++ Hw/src/NLogNSort.java
@@ -1,8 +1,13 @@
 import java.util.Arrays;
 
 public class NLogNSort {
-    public void sort(int[] data)
+    public void MergeSort(int[] data, int L,int R)
     {
-        
+        if (R <= L) return;
+
+        int M = (L+R)/2;
+        MergeSort(data, L, M);
+        MergeSort(data, M+1, R);
+        Merge(data, L, M, R);
     }
 }
