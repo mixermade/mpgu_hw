9b83c5d1f4b3e25a6f28db61b8798fb820e21e47
mixermade <79300664+mixermade@users.noreply.github.com>
2021-04-16 10:37:02 +0300

    Update binaryTreeTests.java

--- Hw/src/binaryTreeTests.java
+++ Hw/src/binaryTreeTests.java
@@ -9,3 +9,32 @@ import org.junit.jupiter.api.Test;
 
             assertEquals("3", tree.search("3"));
         }
+
+        @Test
+        public void Min_findsMinOfTree_minEquals1() {
+            binaryTree tree = new binaryTree();
+
+            tree.insert( "33");
+            tree.insert( "23");
+            tree.insert( "1");
+            tree.insert( "53");
+
+
+            assertEquals("1", tree.Min(tree.root));
+        }
+
+        @Test
+        public void maxValue_findsMaxOfTree_minEquals1() {
+            binaryTree tree = new binaryTree();
+
+            tree.insert( "55");
+            tree.insert( "2");
+            tree.insert( "3");
+            tree.insert( "7");
+            tree.insert( "34");
+
+
+            assertEquals("34", tree.Max(tree.root));
+        }
+    }
+
