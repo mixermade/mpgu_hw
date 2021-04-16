0a258f26ce51c6dc496cfaa95106f1e68d7a0e4a
mixermade <79300664+mixermade@users.noreply.github.com>
2021-04-16 08:53:03 +0300

    Create BSTitem.java

---
+++ Hw/src/BSTitem.java
@@ -0,0 +1,11 @@
+public class BSTitem {
+    String key;
+    BSTitem p;
+    BSTitem left;
+    BSTitem right;
+    BSTitem(String key) {
+        this.key = key;
+        right = null;
+        left = null;
+    }
+}
