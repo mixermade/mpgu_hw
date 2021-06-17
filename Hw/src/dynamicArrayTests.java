55a46102d6d206917db52d612c609cae1732d469
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-17 20:23:15 +0300

    default

--- Hw/src/dynamicArrayTests.java
+++ Hw/src/dynamicArrayTests.java
@@ -1,3 +1,12 @@
-public class dynamicArrayTests{
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class dynamicArrayTests extends Assertions{
+    @Test
+    public void init_sizeDefault() {
+        dynamicArray array = new dynamicArray();
+        int expected = array.starterSize;
 
+        int actual = array.size();
+        assertEquals(expected, actual);
+    }
 }
