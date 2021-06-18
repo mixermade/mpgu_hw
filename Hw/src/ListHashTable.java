90784f1724395594ebd4c8c846f4f8c9bf3891af
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:30:09 +0300

    Update ListHashTable.java

--- Hw/src/ListHashTable.java
+++ Hw/src/ListHashTable.java
@@ -2,3 +2,23 @@ public class ListHashTable {
     int[] arr;
     int capacity;
 
+    public ListHashTable(int capacity) {
+        this.capacity = nextPrime(capacity);
+        arr = new int[this.capacity];
+    }
+
+
+    private static boolean isPrime(int n) {
+        if (n == 2 || n == 3) {
+            return true;
+        }
+        if (n == 1 || n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
