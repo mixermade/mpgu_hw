0eccf6e573d51c5b1cb4a875f6323b9b43e98359
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:30:45 +0300

    Update ListHashTableTest.java

--- Hw/src/ListHashTableTest.java
+++ Hw/src/ListHashTableTest.java
@@ -2,3 +2,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class ListHashTableTest extends Assertions {
+    @Test
+    public void insert_insertionOfElement11_equalsTrue() {
+        ListHashTable table = new ListHashTable(10);
+
+        table.insert(4);
+        table.insert(11);
+        table.insert(8);
+
+        assertEquals(true, table.search(11));
+    }
+
