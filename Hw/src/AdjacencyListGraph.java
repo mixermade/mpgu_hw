4f49141c0d43f5918a93f892b7c44e6f3575fcca
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:11:46 +0300

    Update AdjacencyListGraph.java

--- Hw/src/AdjacencyListGraph.java
+++ Hw/src/AdjacencyListGraph.java
@@ -1,3 +1,18 @@
 import java.util.*;
 
     public class AdjacencyListGraph {
+        int n;
+        LinkedList<LinkedList<Integer> > adj;
+
+        private String outOfBorders = "-_-";
+
+        private void throwsOutOfBordersExc() {
+            throw new RuntimeException(outOfBorders);
+        }
+
+        public AdjacencyListGraph(int n0) {
+            n = n0;
+            adj = new LinkedList<LinkedList<Integer>>();
+            for (int i = 0; i < n; ++i) {
+                adj.add(new LinkedList<Integer>());
+            }
