e4e2638422c2cf088f1b201b70327d8e02636f8a
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:31:53 +0300

    Update OffsetHashTableTest.java

--- Hw/src/OffsetHashTableTest.java
+++ Hw/src/OffsetHashTableTest.java
@@ -2,3 +2,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class OffsetHashTableTest extends Assertions {
+    @Test
+    public void insert_insertionKey5_getsValue7() {
+        OffsetHashTable<Integer, Integer> table = new OffsetHashTable<>();
+
+        table.insert(5, 7);
+
+        assertEquals(7, table.get(5));
+    }
+
