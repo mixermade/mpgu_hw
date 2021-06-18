668d36c5fa3db9fcd7559ca78f0d0fd630fdb387
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:12:14 +0300

    Update AdjacencyListGraphTest.java

--- Hw/src/AdjacencyListGraphTest.java
+++ Hw/src/AdjacencyListGraphTest.java
@@ -14,3 +14,20 @@ public class AdjacencyListGraphTest extends Assertions {
         assertEquals(true, graph.hasEdge(2,4));
         assertEquals(true, graph.hasEdge(3,4));
     }
+
+    @Test
+    public void removeEdge_removesAllEdges_oldEdgesEqualsFalse() {
+        AdjacencyListGraph graph = new AdjacencyListGraph(5);
+
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+        graph.removeEdge(1, 2);
+        graph.removeEdge(2, 4);
+        graph.removeEdge(3, 4);
+
+        assertEquals(false, graph.hasEdge(1, 2));
+        assertEquals(false, graph.hasEdge(2, 4));
+        assertEquals(false, graph.hasEdge(3, 4));
+    }
+
