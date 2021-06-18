b55777d4c863352c30d86c341a863112ad942023
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 07:42:59 +0300

    Update Dequeue.java

--- Hw/src/Dequeue.java
+++ Hw/src/Dequeue.java
@@ -1,3 +1,10 @@
 public class Dequeue<T> {
     DLL<T> list = new DLL<T>();
 
+    public void add_front(T value) {
+        list.append(value);
+    }
+
+    public void add_back(T value) {
+        list.push(value);
+    }
