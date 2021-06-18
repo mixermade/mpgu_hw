e59b5c0924656a6ada504a9988c91293b283275d
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:13:15 +0300

    Update AdjacencyMatrixGraphTest.java

--- Hw/src/AdjacencyMatrixGraphTest.java
+++ Hw/src/AdjacencyMatrixGraphTest.java
@@ -15,3 +15,19 @@ public class AdjacencyMatrixGraphTest extends Assertions {
         assertEquals(true, graph.hasEdge(2,4));
         assertEquals(true, graph.hasEdge(3,4));
     }
+
+    @Test
+    public void removeEdge_removesAllEdges_oldEdgesEqualsFalse() {
+        AdjacencyMatrixGraph graph = new AdjacencyMatrixGraph(5);
+
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+        graph.removeEdge(1, 2);
+        graph.removeEdge(2, 4);
+        graph.removeEdge(3, 4);
+
+        assertEquals(false, graph.hasEdge(1, 2));
+        assertEquals(false, graph.hasEdge(2, 4));
+        assertEquals(false, graph.hasEdge(3, 4));
+    }
