6eece199e730647c9f2aa63b04f97c94c0df70df
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 07:43:36 +0300

    Update DequeueTest.java

--- Hw/src/DequeueTest.java
+++ Hw/src/DequeueTest.java
@@ -27,6 +27,17 @@ public class DequeueTest extends Assertions {
     }
 
     @Test
+    public void deck_addFront_ValueTest() {
+        Dequeue<Integer> deck = new Dequeue<Integer>();
+
+        deck.add_front(1);
+
+        int expected = 1;
+        int actual = deck.peek_front();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     public void deck_peekFront_ValueTest() {
         Dequeue<Integer> deck = new Dequeue<Integer>();
 
