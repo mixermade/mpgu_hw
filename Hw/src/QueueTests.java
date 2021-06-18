07096d0ef8ec343391f0f05dba47538a9766501f
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 06:33:23 +0300

    Update QueueTests.java

--- Hw/src/QueueTests.java
+++ Hw/src/QueueTests.java
@@ -3,4 +3,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class QueueTests extends Assertions {
+
+    @Test
+    public void enqueue_check_data() {
+        Queue<Integer> queue = new Queue<Integer>();
+        int expected = 10;
+
+        queue.enqueue(10);
+
+        int actual = queue.peek();
+        assertEquals(expected, actual);
+
+        expected = 10;
+        queue.enqueue(20);
+        actual = queue.peek();
+        assertEquals(expected, actual);
+    }
+
 }
