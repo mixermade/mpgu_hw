fdf9d8f78db5c9341c7be5d0b9ca10c28fff8e0f
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:13:47 +0300

    Update EdgesListGraph.java

--- Hw/src/EdgesListGraph.java
+++ Hw/src/EdgesListGraph.java
@@ -10,3 +10,17 @@ import java.util.LinkedList;
                 this.num = num;
                 edgeList = new LinkedList < > ();
             }
+
+            public LinkedList < Edge > getEdges() {
+                return edgeList;
+            }
+        }
+
+        public static class Edge {
+            private int weight;
+            private Node destVertex;
+
+            public Edge(Node dest, int w) {
+                this.destVertex = dest;
+                this.weight = w;
+            }
