0dbdaa704e8a3f113ec23061655a9a1e3d84519e
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:37:52 +0300

    Update AVLNode.java

--- Hw/src/AVLNode.java
+++ Hw/src/AVLNode.java
@@ -8,3 +8,32 @@ public class AVLNode {
         this.key = key;
         this.left = this.right = null;
     }
+
+    public AVLNode getLeft() {
+        return left;
+    }
+
+    public AVLNode getRight() {
+        return right;
+    }
+
+    public void setLeft(AVLNode nLeft) {
+        this.left = nLeft;
+    }
+
+    public void setRight(AVLNode nRight) {
+        this.right = nRight;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public int getHeight(){return height;}
+
+    public void setHeight(int height){this.height = height;}
+}
