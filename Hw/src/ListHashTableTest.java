9b1c51d6d2df18dae234952994b2345b8a96bfd9
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:30:48 +0300

    Update ListHashTableTest.java

--- Hw/src/ListHashTableTest.java
+++ Hw/src/ListHashTableTest.java
@@ -13,3 +13,14 @@ public class ListHashTableTest extends Assertions {
         assertEquals(true, table.search(11));
     }
 
+    @Test
+    public void delete_searchFor11_equalsFalse() {
+        ListHashTable table = new ListHashTable(10);
+
+        table.insert(4);
+        table.insert(11);
+        table.insert(8);
+        table.delete(11);
+
+        assertEquals(false, table.search(11));
+    }
