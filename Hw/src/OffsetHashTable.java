b0477a951c34f60a08124ad1c78b02dbd4da9d9f
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:31:14 +0300

    Update OffsetHashTable.java

--- Hw/src/OffsetHashTable.java
+++ Hw/src/OffsetHashTable.java
@@ -7,3 +7,20 @@ class HashNode<K, V> {
     final int hashCode;
 
 
+    HashNode<K, V> next;
+
+
+    public HashNode(K key, V value, int hashCode) {
+        this.key = key;
+        this.value = value;
+        this.hashCode = hashCode;
+    }
+}
+
+public class OffsetHashTable<K, V> {
+    private ArrayList<HashNode<K, V> > array;
+
+
+    private int numBuckets;
+
+
