5bea0e1924b32d32a7fad92c0aa2ee6b9357c39a
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 07:43:20 +0300

    Create DequeueTest.java

---
+++ Hw/src/DequeueTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DequeueTest extends Assertions {
+
+    public void deck_peekFront_ValueTest() {
+        Dequeue<Integer> deck = new Dequeue<Integer>();
+
+        deck.add_front(1);
+
+        int expected = 1;
+        int actual = deck.peek_front();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deck_popFront_sizeTest() {
+        Dequeue<Integer> deck = new Dequeue<Integer>();
+
+        deck.add_front(1);
+        deck.add_front(1);
+        deck.pop_front();
+
+        int expected = 1;
+        int actual = deck.get_size();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deck_popFront_ValueTest() {
+        Dequeue<Integer> deck = new Dequeue<Integer>();
+
+        deck.add_front(1);
+        deck.add_front(2);
+        deck.pop_front();
+
+        int expected = 1;
+        int actual = deck.peek_front();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deck_popBack_sizeTest() {
+        Dequeue<Integer> deck = new Dequeue<Integer>();
+
+        deck.add_back(1);
+        deck.add_back(1);
+        deck.pop_back();
+
+        int expected = 1;
+        int actual = deck.get_size();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deck_popBack_ValueTest() {
+        Dequeue<Integer> deck = new Dequeue<Integer>();
+        deck.add_back(1);
+        deck.add_back(2);
+        deck.pop_back();
+
+        int expected = 1;
+        int actual = deck.peek_back();
+        Assertions.assertEquals(expected, actual);
+    }
+}
