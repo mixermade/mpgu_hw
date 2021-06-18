0a5ba1edf3f043a3970e8b42c5304429972109a7
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 06:33:05 +0300

    things

--- Hw/src/Queue.java
+++ Hw/src/Queue.java
@@ -1,16 +1,23 @@
 public class Queue<T> {
-
-
-
-
-
-
-
-
-
-
     dynamicArray<T> queue = new dynamicArray<T>();
     int size = 0;
 
+    public void enqueue(T value) {
+        queue.add(value);
+        size++;
+    }
+
+    public void dequeue() {
+        queue.remove(0);
+        size--;
+    }
+
+    public T peek() {
+        return queue.get(0);
+    }
+
+    public int size() {
+        return size;
+    }
 
 }
