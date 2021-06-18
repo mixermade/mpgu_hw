04e83370d0285b2b9faade3faf8ff1514d690bd7
mixermade <79300664+mixermade@users.noreply.github.com>
2021-06-18 08:12:10 +0300

    Update AdjacencyListGraphTest.java

--- Hw/src/AdjacencyListGraphTest.java
+++ Hw/src/AdjacencyListGraphTest.java
@@ -2,3 +2,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class AdjacencyListGraphTest extends Assertions {
+    @Test
+    public void addEdge_adds3NewEdges_allNewEdgesEqualsTrue() {
+        AdjacencyListGraph graph = new AdjacencyListGraph(5);
+
+        graph.addEdge(1, 2);
+        graph.addEdge(2,4);
+        graph.addEdge(3,4);
+
+        assertEquals(true, graph.hasEdge(1,2));
+        assertEquals(true, graph.hasEdge(2,4));
+        assertEquals(true, graph.hasEdge(3,4));
+    }
